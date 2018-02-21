9d503da7a4c69c4b571414ac9b3b71400e6c5371
abou01 <abou-dukuray@hotmail.fr>
2018-02-21 14:59:48 +0100

    Page Prono

---
+++ src/app/prono-page/prono-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prono-page',
+  templateUrl: './prono-page.component.html',
+  styleUrls: ['./prono-page.component.css']
+})
+export class PronoPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

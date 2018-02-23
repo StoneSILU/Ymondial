92c9c61fd72976d37bfc70f14987d7b602a5744f
Anthony Bemba <anthony.bemba@yahoo.fr>
2018-02-23 15:33:48 +0100

    page login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Prono Football School';
+  title = 'Pronos Football School';
 }
